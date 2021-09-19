51c8e491c3d0a640f3a0104a2b120da38d6d452b
Washington Ribeiro <washington.s@escolar.ifrn.edu.br>
2021-09-19 15:39:39 -0300

    Adding setUpAnalytics in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare let gtag: Function;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: []
 })
-export class AppComponent {
-  title = 'washingtonsr-blog';
+export class AppComponent implements OnInit {
+    constructor(private router: Router) {
+    }
+
+    ngOnInit() {
+        this.setUpAnalytics();
+    }
+
+    setUpAnalytics() {
+
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd))
+            .subscribe((event: any) => {
+                gtag('config', 'G-MPNDWGS0BZ',
+                    {
+                        page_path: event.urlAfterRedirects
+                    }
+                );
+            });
+    }
 }
